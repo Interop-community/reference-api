47ad9a1e3deb72c84eb5f62696e00bf46bfa030c
Chit Zin Win <chit.win@mihin.org>
2023-04-12 14:46:09 -0400

    enfored to retain subscription for broward POC

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM openjdk:11.0.7-jdk-slim
+FROM 745222113226.dkr.ecr.us-east-1.amazonaws.com/interop/api:develop
+# openjdk:11.0.7-jdk-slim
 ADD reference-api-webapp/target/hspc-reference-api-webapp-*.jar app.jar
-RUN apt-get update -y && apt-get dist-upgrade -y
-RUN apt-get install -y default-mysql-client
+# RUN apt-get update -y && apt-get dist-upgrade -y
+# RUN apt-get install -y default-mysql-client
 ENV JAVA_OPTS="-Xmx1536m -Djava.security.egd=file:/dev/./urandom"
 ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -jar app.jar" ]
--- reference-api-webapp/src/main/java/org/logicahealth/platform/api/config/SubscriptionProcessorConfig.java
+++ reference-api-webapp/src/main/java/org/logicahealth/platform/api/config/SubscriptionProcessorConfig.java
@@ -1,5 +1,7 @@
 package org.logicahealth.platform.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ca.uhn.fhir.jpa.subscription.match.deliver.email.IEmailSender;
@@ -8,11 +10,17 @@ import ca.uhn.fhir.jpa.subscription.model.CanonicalSubscription;
 import ca.uhn.fhir.jpa.subscription.model.ResourceDeliveryMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
+import ca.uhn.fhir.jpa.subscription.match.registry.SubscriptionRegistry;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 
 public class SubscriptionProcessorConfig extends ca.uhn.fhir.jpa.subscription.match.config.SubscriptionProcessorConfig {
 
-    @Override
-    @Bean
+  private static final Logger myLog = LoggerFactory.getLogger(SubscriptionProcessorConfig.class);
+  
+  @Override
+  @Bean
 	@Scope("prototype")
 	public SubscriptionDeliveringEmailSubscriber subscriptionDeliveringEmailSubscriber(IEmailSender theEmailSender) {
         return (SubscriptionDeliveringEmailSubscriber) new mySubscriptionDeliveringEmailSubscriber(theEmailSender);
@@ -39,5 +47,26 @@ public class SubscriptionProcessorConfig extends ca.uhn.fhir.jpa.subscription.ma
 
         super.handleMessage(theMessage);
         }
+  }
+
+
+    @Override
+    @Bean
+    public SubscriptionRegistry subscriptionRegistry() {
+        return (SubscriptionRegistry) new mySubscriptionRegistry();
+    }
+
+  public class mySubscriptionRegistry extends SubscriptionRegistry{
+
+    @Override
+    public void unregisterSubscriptionIfRegistered(String theSubscriptionId) {
+
+      myLog.info("Blocking Unregistration Subscription: " + theSubscriptionId);
+
+        // System.out.printf("%s is calling to unregister %s\n", 
+        //     Arrays.toString(Thread.currentThread().getStackTrace()).replace(',','\n'), theSubscriptionId );
+
+    }
+
   }
 }
